009ea6eaafea7414748b855bac3912ebc2df4c2f
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2024-02-21 20:12:48 +0100

    feature: Finish day1

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -2,7 +2,7 @@
 
 use std::ops::Bound;
 use std::path::Path;
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 use anyhow::Result;
@@ -37,8 +37,13 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 
 impl MemTable {
     /// Create a new mem-table.
-    pub fn create(_id: usize) -> Self {
-        unimplemented!()
+    pub fn create(id: usize) -> Self {
+        Self {
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            id,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     /// Create a new mem-table with WAL
@@ -68,16 +73,35 @@ impl MemTable {
     }
 
     /// Get a value by key.
-    pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+    pub fn get(&self, key: &[u8]) -> Option<Bytes> {
+        self.map.get(key).map(|f| f.value().clone())
     }
 
     /// Put a key-value pair into the mem-table.
     ///
     /// In week 1, day 1, simply put the key-value pair into the skipmap.
     /// In week 2, day 6, also flush the data to WAL.
-    pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        self.update_size_on_put(key, value);
+        let key = Bytes::copy_from_slice(key);
+        let value = Bytes::copy_from_slice(value);
+        self.map.insert(key, value);
+        Ok(())
+    }
+
+    fn update_size_on_put(&self, key: &[u8], value: &[u8]) {
+        let previous_entry = self.get(key);
+        if value.is_empty() && previous_entry.is_some() {
+            // we are removing value, so we need to decrease `approximate_size`
+            self.approximate_size
+                .fetch_sub(previous_entry.unwrap().len(), Ordering::SeqCst);
+        } else {
+            // otherwise we are adding / overwriting a value
+            // (approximate size accepts overriden value counted twice)
+            let added_size = key.len() + value.len();
+            self.approximate_size
+                .fetch_add(added_size, Ordering::SeqCst);
+        }
     }
 
     pub fn sync_wal(&self) -> Result<()> {
